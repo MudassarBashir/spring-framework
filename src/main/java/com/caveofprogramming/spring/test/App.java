03e4b4ea6ebe636acee7cc3646f8628c0df72e50
Moe B <moe.bashir@logianalytics.com>
2016-06-29 12:13:21 -0400

    Using ClassPathXMLApplicationContext. Note, this did not work for me as it does by default in Eclipse. I had to change a line in an IDEA configuration file according to the second answer on this page:
    http://stackoverflow.com/questions/31076576/xml-files-are-not-copied-to-target-intellij-idea

--- src/main/java/com/caveofprogramming/spring/test/App.java
+++ src/main/java/com/caveofprogramming/spring/test/App.java
@@ -1,13 +1,14 @@
 package com.caveofprogramming.spring.test;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new FileSystemXmlApplicationContext("src\\main\\java\\com\\caveofprogramming\\spring\\test\\com\\caveofprogramming\\spring\\test\\beans\\beans.xml");
+		ApplicationContext context =
+                new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
 		Person person = (Person)context.getBean("person");
 		person.speak();
@@ -17,7 +18,7 @@ public class App {
          * Here is what it looks like when we do close it. Notice that the close() method belongs to
          * FileSystemXmlApplicationContext and not ApplicationContext, hence the casting is needed.
          */
-		((FileSystemXmlApplicationContext) context).close();
+		((ClassPathXmlApplicationContext) context).close();
 	}
 
 }
