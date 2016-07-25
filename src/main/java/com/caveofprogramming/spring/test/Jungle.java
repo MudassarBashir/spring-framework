a433133b276a3881d837a0ab961620eb7508c9f7
Moe B <moe.bashir@logianalytics.com>
2016-07-25 16:26:55 -0400

    Properties. Strings inside a Map.

--- src/main/java/com/caveofprogramming/spring/test/Jungle.java
+++ src/main/java/com/caveofprogramming/spring/test/Jungle.java
@@ -1,36 +1,26 @@
 package com.caveofprogramming.spring.test;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mbashir on 7/24/2016.
  */
 public class Jungle {
 
-    private Animal largest;
-    private List<Animal> animals;
+    private Map<String, String> foods = new HashMap<String, String>();
 
-    public Animal getLargest() {
-        return largest;
-    }
-
-    public void setLargest(Animal largest) {
-        this.largest = largest;
-    }
-
-    public List<Animal> getAnimals() {
-        return animals;
-    }
-
-    public void setAnimals(List<Animal> animals) {
-        this.animals = animals;
+    public void setFoods(Map<String, String> foods) {
+        this.foods = foods;
     }
 
     @Override
     public String toString() {
-        return "Jungle{" +
-                "largest=" + largest +
-                ", animals=" + animals +
-                '}';
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : foods.entrySet()) {
+            sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
+        }
+        return sb.toString();
     }
 }
