3d0494218f746d128018fc78b5d3aa942452438c
Moe B <moe.bashir@logianalytics.com>
2016-07-28 17:40:43 -0400

    Autowire annotation can be place on the line before a setter method or before the property declaration.

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,15 +4,19 @@ package com.caveofprogramming.spring.test;
  * Dummy implementation of logger.
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
 
+	@Autowired
 	private ConsoleWriter consoleWriter;
+
 	private FileWriter fileWriter;
 
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
-
+	@Autowired
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
