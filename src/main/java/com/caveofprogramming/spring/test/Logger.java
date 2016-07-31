690a77d803d98b53cb70135abf18dc5b5f715da9
Moe B <moe.bashir@logianalytics.com>
2016-07-30 20:57:12 -0400

    Autowiring with constructor.

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -8,15 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Logger {
 
-	@Autowired
-	private ConsoleWriter consoleWriter;
 
+	private ConsoleWriter consoleWriter;
 	private FileWriter fileWriter;
 
-	public void setConsoleWriter(ConsoleWriter writer) {
+    @Autowired
+    public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+        this.consoleWriter = consoleWriter;
+        this.fileWriter = fileWriter;
+    }
+
+    public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
-	@Autowired
+
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
