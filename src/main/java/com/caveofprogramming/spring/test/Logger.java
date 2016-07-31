f3ce32d6bc4432017d33cee3a9fa2dd409904f27
Moe B <moe.bashir@logianalytics.com>
2016-07-30 21:01:10 -0400

    Autowiring without setters or constructor.

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -8,23 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Logger {
 
-
+    @Autowired
 	private ConsoleWriter consoleWriter;
 	@Autowired
     private FileWriter fileWriter;
-
-    @Autowired
-    public Logger(ConsoleWriter consoleWriter) {
-        this.consoleWriter = consoleWriter;
-    }
-
-    public void setConsoleWriter(ConsoleWriter writer) {
-		this.consoleWriter = writer;
-	}
-
-	public void setFileWriter(FileWriter fileWriter) {
-		this.fileWriter = fileWriter;
-	}
 	
 	public void writeFile(String text) {
 		fileWriter.write(text);
