5407e649230f957ed0d36cb0aa3bf476d4256d05
Moe B <moe.bashir@logianalytics.com>
2016-07-31 21:58:16 -0400

    JSR-250's way of declaring init and destroy methods via respective annotations. The methods themselves can be called anything besides init and destroy so long as they are decorated with the annotations accordingly.

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,6 +4,8 @@ package com.caveofprogramming.spring.test;
  * Dummy implementation of logger.
  */
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
 public class Logger {
@@ -25,7 +27,17 @@ public class Logger {
     public void writeFile(String text) {
 		fileWriter.write(text);
 	}
-	
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Init method.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destroy method.");
+    }
+
 	public void writeConsole(String text) {
         if (consoleWriter != null) {
             consoleWriter.write(text);
