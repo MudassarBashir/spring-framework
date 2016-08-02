1996b0d87c9694f8f260b6872ce68ab14e2f78a4
Moe B <moe.bashir@logianalytics.com>
2016-08-02 16:23:07 -0400

    Some SPEL operators, example usage. Static method calling part 2.

--- src/main/java/com/caveofprogramming/spring/test/Robot.java
+++ src/main/java/com/caveofprogramming/spring/test/Robot.java
@@ -15,12 +15,12 @@ public class Robot {
 		System.out.println(id + ": " + speech);
 	}
     @Autowired
-	public void setId(@Value("#{randomText.getText().length()}") String id) {
+	public void setId(@Value("#{randomText.getText()?.length()}") String id) {
 		this.id = id;
 	}
 
 	@Autowired
-	public void setSpeech(@Value("#{T(Math).PI}") String speech) {
+	public void setSpeech(@Value("#{T(Math).sin(T(Math).PI/4)}") String speech) {
 		this.speech = speech;
 	}
 	
