b5152aaeacd150b11e7c711d3a59385348a19a74
Moe B <moe.bashir@logianalytics.com>
2016-08-12 12:07:20 -0400

    Create Offers DAO to interact with DB.

---
+++ src/main/java/com/caveofprogramming/spring/dao/OffersDAO.java
@@ -0,0 +1,17 @@
+package com.caveofprogramming.spring.dao;
+
+import com.caveofprogramming.spring.model.Offer;
+
+import java.util.List;
+
+/**
+ * Created by mbashir on 8/12/2016.
+ */
+public class OffersDAO {
+
+    public List<Offer> getOffers() {
+
+        return null;
+    }
+
+}
