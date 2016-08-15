6e7b6f3af2af874fb210d8c2ca858ef6f35e8bef
Moe B <moe.bashir@logianalytics.com>
2016-08-15 15:13:14 -0400

    Using JdbcTemplate to finish configuring the connection pooling.

--- src/main/java/com/caveofprogramming/spring/dao/OffersDAO.java
+++ src/main/java/com/caveofprogramming/spring/dao/OffersDAO.java
@@ -1,13 +1,25 @@
 package com.caveofprogramming.spring.dao;
 
 import com.caveofprogramming.spring.model.Offer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
  * Created by mbashir on 8/12/2016.
  */
+@Component
 public class OffersDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     public List<Offer> getOffers() {
 
