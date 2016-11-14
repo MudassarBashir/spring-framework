80c401b89ded429b2c6c34c18535073ac82b6f7d
Moe B <moeb59@gmail.com>
2016-11-14 11:59:16 -0500

    Vid 53: Creating a controller. Done.

--- src/main/java/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/main/java/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,7 +1,16 @@
 package com.caveofprogramming.spring.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Moe B. on 11/14/2016.
  */
+@Controller
 public class OffersController {
+
+    @RequestMapping("/")
+    public String showHome() {
+        return "home"; // Names the view the use.
+    }
 }
