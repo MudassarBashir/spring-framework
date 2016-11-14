e3df64adf9375354f1b74aa8783e9a2a7e0e594f
Moe B <moeb59@gmail.com>
2016-11-14 14:28:42 -0500

    Vid 55: Setting session attributes.

--- src/main/java/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/main/java/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -3,6 +3,8 @@ package com.caveofprogramming.spring.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by Moe B. on 11/14/2016.
  */
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OffersController {
 
     @RequestMapping("/")
-    public String showHome() {
+    public String showHome(HttpSession session) {
+        session.setAttribute("name", "Boris");
         return "home"; // Names the view the use.
     }
 }
--- src/main/webapp/WEB-INF/jsps/home.jsp
+++ src/main/webapp/WEB-INF/jsps/home.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello Spring World!</h2>
+<p>Session attribute from controller: <%= session.getAttribute("name")%></p>
 </body>
 </html>
