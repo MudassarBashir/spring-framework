8474b286adac1453fdf59b74d7b8fb31c97f32e4
Moe B <moeb59@gmail.com>
2016-12-01 18:01:36 -0500

    Vid 75: Using an error web page to show a customer an error occured, which is much friendlier than showing a raw exception.

--- src/main/java/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/main/java/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -3,10 +3,12 @@ package com.caveofprogramming.spring.web.controllers;
 import com.caveofprogramming.spring.web.dao.Offer;
 import com.caveofprogramming.spring.web.service.OffersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +29,11 @@ public class OffersController {
     public String showTest(Model model, @RequestParam("id") String id) {
         System.out.println("Id is: " + id);
         return "home";
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public String handleDatabaseException(DataAccessException ex) {
+        return "error";
     }
 
     @RequestMapping("/offers")
