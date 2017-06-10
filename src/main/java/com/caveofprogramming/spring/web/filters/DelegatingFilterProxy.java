1392f1351b24531d3d820b03b558323663ec674d
Moe B <moeb59@gmail.com>
2017-06-10 16:44:35 -0400

    Vid 92: Adding a spring security filter.
    Removed 'generic' filter added in last lesson and added a spring security filter instead.

--- src/main/java/com/caveofprogramming/spring/web/filters/DelegatingFilterProxy.java
+++ src/main/java/com/caveofprogramming/spring/web/filters/DelegatingFilterProxy.java
@@ -2,19 +2,17 @@ package com.caveofprogramming.spring.web.filters;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
- * Created by Moe B. on 6/9/2017.
+ * Created by Moe B. on 6/10/2017.
  */
-@WebFilter(filterName = "TestFilter")
-public class TestFilter implements Filter {
+@WebFilter(filterName = "springSecurityFilterChain")
+public class DelegatingFilterProxy implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        System.out.println(((HttpServletRequest) req).getRequestURI());
         chain.doFilter(req, resp);
     }
 
