8b5279e10863e6ed3c49ca01eeaf7dae75f788f2
Moe B <moeb59@gmail.com>
2017-09-07 16:28:02 -0400

    101: Checking for duplicate usernames.

--- src/main/java/com/caveofprogramming/spring/web/controllers/LoginController.java
+++ src/main/java/com/caveofprogramming/spring/web/controllers/LoginController.java
@@ -4,6 +4,7 @@ import com.caveofprogramming.spring.web.dao.Offer;
 import com.caveofprogramming.spring.web.dao.User;
 import com.caveofprogramming.spring.web.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -39,7 +40,17 @@ public class LoginController {
         }
         user.setEnabled(true);
         user.setAuthority("user");
-        usersService.create(user);
+
+        if (usersService.exists(user.getUsername())) {
+            result.rejectValue("username", "duplicatekey.user.username", "This username already exists.");
+            return "newaccount";
+        }
+        try {
+            usersService.create(user);
+        } catch (DuplicateKeyException e) {
+            result.rejectValue("username", "duplicatekey.user.username", "This username already exists.");
+            return "newaccount";
+        }
         return "accountcreated";
     }
 }
