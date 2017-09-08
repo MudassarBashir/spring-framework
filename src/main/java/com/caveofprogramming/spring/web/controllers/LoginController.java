15fd64c351136d0bd9f4ccb28fd3ab0004b1b31e
Moe B <moeb59@gmail.com>
2017-09-07 22:11:53 -0400

    102: Putting messages in property files.

--- src/main/java/com/caveofprogramming/spring/web/controllers/LoginController.java
+++ src/main/java/com/caveofprogramming/spring/web/controllers/LoginController.java
@@ -19,8 +19,12 @@ import javax.validation.Valid;
 @Controller
 public class LoginController {
 
-    @Autowired
     private UsersService usersService;
+
+    @Autowired
+    public void setUsersService(UsersService usersService) {
+        this.usersService = usersService;
+    }
 
     @RequestMapping("/login")
     public String showLogin() {
@@ -29,28 +33,35 @@ public class LoginController {
 
     @RequestMapping("/newaccount")
     public String showNewAccount(Model model) {
+
         model.addAttribute("user", new User());
         return "newaccount";
     }
 
+
     @RequestMapping(value = "/createaccount", method = RequestMethod.POST)
     public String createAccount(@Valid User user, BindingResult result) {
+
         if (result.hasErrors()) {
             return "newaccount";
         }
-        user.setEnabled(true);
+
         user.setAuthority("user");
+        user.setEnabled(true);
 
         if (usersService.exists(user.getUsername())) {
-            result.rejectValue("username", "duplicatekey.user.username", "This username already exists.");
+            result.rejectValue("username", "DuplicateKey.user.username");
             return "newaccount";
         }
+
         try {
             usersService.create(user);
         } catch (DuplicateKeyException e) {
-            result.rejectValue("username", "duplicatekey.user.username", "This username already exists.");
+            result.rejectValue("username", "DuplicateKey.user.username");
             return "newaccount";
         }
+
+
         return "accountcreated";
     }
 }
